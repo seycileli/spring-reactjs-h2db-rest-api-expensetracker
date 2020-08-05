9d821e08e67eeadc9be41739eb297368a108a3a4
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-05 13:11:32 -0400

    Create ExpenseController.java

---
+++ my-app/main/java/controller/ExpenseController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import model.Expense;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import repository.ExpenseRepository;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ExpenseController {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @GetMapping("/expenses")
+    List<Expense> getExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    @DeleteMapping("/expenses/{id}")
+    ResponseEntity<?> deleteExpenses(@PathVariable Long id) {
+        expenseRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/expenses")
+    ResponseEntity<Expense> createExpense(@Valid @RequestBody Expense expense)
+            throws URISyntaxException {
+        Expense result = expenseRepository.save(expense);
+        return ResponseEntity.created(new URI("/api/expenses" +
+                result.getId())).body(result);
+    }
+
+
+}
