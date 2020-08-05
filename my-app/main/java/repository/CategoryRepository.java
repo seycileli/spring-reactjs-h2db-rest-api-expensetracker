7a72ddf6234b687ccd42a5883aad2f36408a98ca
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-05 13:13:39 -0400

    Create CategoryRepository.java

---
+++ my-app/main/java/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package repository;
+
+import model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
+
+}
