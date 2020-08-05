6d73e34c18f4330ab18b283c2e169fbf56197380
Séyhan A. Cileli <52051065+seycileli@users.noreply.github.com>
2020-08-05 13:13:10 -0400

    Create User.java

---
+++ my-app/main/java/model/User.java
@@ -0,0 +1,62 @@
+package model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+    @Id @GeneratedValue
+    @Basic @Column
+    private Long id;
+
+    @Basic @Column
+    private String name;
+
+    @Basic @Column
+    private String email;
+
+    public User() {
+        super();
+    }
+
+    public User(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
